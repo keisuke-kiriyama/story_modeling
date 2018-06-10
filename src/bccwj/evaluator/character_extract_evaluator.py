a554fc649f55b33b4b405c081cf290fd479ca2dd
Keisuke Kiriyama <krkr.xm.rkr7x@gmail.com>
2018-06-10 18:47:45 +0900

    :heavy_plus_sign: 全標本の評価

--- src/bccwj/evaluator/character_extract_evaluator.py
+++ src/bccwj/evaluator/character_extract_evaluator.py
@@ -1,5 +1,6 @@
 import os
 import xml.etree.ElementTree as ET
+from statistics import mean, variance
 import MeCab
 from src.util import settings
 
@@ -37,6 +38,7 @@ class CharacterExtractEvaluator:
                     character_name['common_name'] = suw_text
             if not character_name in character_list and character_name:
                 character_list.append(character_name)
+        self.correct_character_list = character_list
         return character_list
 
     def extract_character_name_with_mecab(self):
@@ -64,18 +66,27 @@ class CharacterExtractEvaluator:
             elif pos == '固有名詞-人名-一般':
                 character_name['common_name'] = morph
             is_previous_name = True
+        self.expectation_character_list = character_list
         return character_list
 
     def eval_extract_characer(self):
         character_list_from_tree = self.extract_character_name_from_mxml_tree()
         character_list_with_mecab = self.extract_character_name_with_mecab()
+        if not character_list_from_tree or not character_list_with_mecab:
+            self.precision = None
+            self.recall = None
+            self.f_measure = None
+            return
         tp, fp = self.positive_expectation_eval(character_list_from_tree, character_list_with_mecab)
         fn = self.negative_expectation_eval(character_list_from_tree, character_list_with_mecab)
         precision = tp / (tp + fp)
         recall = tp / (tp + fn)
-        f_measure = 2 * precision * recall / (precision + recall)
         self.precision = precision
         self.recall = recall
+        if precision + recall == 0:
+            self.f_measure = None
+            return
+        f_measure = 2 * precision * recall / (precision + recall)
         self.f_measure = f_measure
 
     def positive_expectation_eval(self, corrects, expectations):
@@ -96,9 +107,49 @@ class CharacterExtractEvaluator:
         return fn_count
 
 
+def evaluate_character_extraction_with_mecab():
+    literatures_dir_path = os.path.join(settings.BCCWJ_DATA_DIR_PATH, 'Literature')
+    mxml_file_paths = [os.path.join(literatures_dir_path, i) for i in os.listdir(literatures_dir_path)]
+    precisions = []
+    recalls = []
+    f_measures = []
+    items_count = len(mxml_file_paths)
+    for i, mxml_file_path in enumerate(mxml_file_paths):
+        character_extractor_evaluator = CharacterExtractEvaluator(mxml_file_path)
+        character_extractor_evaluator.eval_extract_characer()
+        if character_extractor_evaluator.precision:
+            precisions.append(character_extractor_evaluator.precision)
+        if character_extractor_evaluator.recall:
+            recalls.append(character_extractor_evaluator.recall)
+        if character_extractor_evaluator.f_measure:
+            f_measures.append(character_extractor_evaluator.f_measure)
+        completion_rate = i / items_count
+        print("completion_rate: {:.4f}".format(completion_rate))
+    precision_mean = mean(precisions)
+    precision_variance = variance(precisions)
+    evaluation_precision_item_count = len(precisions)
+    none_precision_count = items_count - evaluation_precision_item_count
+    print('precision Mean: ', precision_mean)
+    print('precision Variance: ', precision_variance)
+    print('num of precisions evaluationed: ', evaluation_precision_item_count)
+    print('cannnot precisions evaluation count: ', none_precision_count)
+    recall_mean = mean(recalls)
+    recall_variance = variance(recalls)
+    evaluation_recall_item_count = len(recalls)
+    none_recall_count = items_count - evaluation_recall_item_count
+    print('recall Mean: ', recall_mean)
+    print('recall Variance: ', recall_variance)
+    print('num of recall evaluationed: ', evaluation_recall_item_count)
+    print('cannnot recall evaluation count: ', none_recall_count)
+    f_measure_mean = mean(f_measures)
+    f_measure_variance = variance(f_measures)
+    evaluation_f_measure_item_count = len(f_measures)
+    none_f_measure_count = items_count - evaluation_f_measure_item_count
+    print('f-measure Mean: ', f_measure_mean)
+    print('f-measure Variance: ', f_measure_variance)
+    print('num of f-measure evaluationed: ', evaluation_f_measure_item_count)
+    print('cannnot f-measure evaluation count: ', none_f_measure_count)
 
 if __name__ == '__main__':
-    mxml_file_path = os.path.join(settings.BCCWJ_DATA_DIR_PATH, 'Literature/PB19_00004.xml')
-    character_extract_evaluator = CharacterExtractEvaluator(mxml_file_path=mxml_file_path)
-    character_extract_evaluator.eval_extract_characer()
+    evaluate_character_extraction_with_mecab()
 
