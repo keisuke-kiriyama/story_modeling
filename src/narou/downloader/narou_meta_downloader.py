47b1a08939b4ba8c2c11134652cebcfcd58c165f
Keisuke Kiriyama <krkr.xm.rkr7x@gmail.com>
2018-06-26 14:39:34 +0900

    :heavy_plus_sign: NCodeのCSVからメタ情報ダウンロード

--- src/narou/downloader/narou_meta_downloader.py
+++ src/narou/downloader/narou_meta_downloader.py
@@ -1,19 +1,43 @@
+import os
 import json
-from urllib.request import urlopen
 import gzip
+from urllib.request import urlopen, HTTPError, URLError
+from time import sleep
 
-def num_of_novels():
-    url = 'http://api.syosetu.com/novelapi/api/?out=json&gzip=5&of=t&lim=1'
-    response = urlopen(url)
-    with gzip.open(response, 'rt', encoding='utf-8') as f:
-        j_raw = f.read()
-        j_obj = json.loads(j_raw)
-    return j_obj[0]['allcount']
+from src.util import settings
 
-def fetch(st, limit):
-    url = "http://api.syosetu.com/novelapi/api/?out=json&of=t-n-u-w-s-bg-g-k-nt-l-gp-f-r-a-ka&gzip=5&st={}&lim={}".format(st, limit)
+def fetch_novel_meta_info(n_code, error_log_file):
+    url = 'https://api.syosetu.com/novelapi/api/?out=json&gzip=5&of=t-n-u-w-s-bg-g-k-nt-e-ga-l-gp-f-r-a-ah-ka-&lim=1&ncode={}'.format(n_code)
+    sleep(3)
+    print("fetch: {}".format(url))
     response = urlopen(url)
-    with gzip.open(response, 'rt', encoding='utf-8') as f:
-        j_raw = f.read()
-    return json.loads(j_raw)
+    try:
+        with gzip.open(response, 'rt', encoding='utf-8') as f:
+            j_raw = f.read()
+            j_obj = json.loads(j_raw)
+            novel_meta = j_obj[1]
+    except:
+        error_log_file.write(n_code)
+    return novel_meta
+
+def fetch_novel_meta_from_ncode(ncode_file_path, output_file_path, error_log_file_path):
+    ncode_file = open(ncode_file_path, 'r')
+    output_file = open(output_file_path, 'w')
+    error_log_file = open(error_log_file_path, 'w')
+    lines = ncode_file.readlines()
+    for line in lines:
+        n_code = line.split(',')[1].replace('"', '').strip()
+        meta = fetch_novel_meta_info(n_code, error_log_file)
+        json.dump(meta, output_file)
+    ncode_file.close()
+    output_file.close()
+    error_log_file.close()
+
+if __name__ == '__main__':
+    csv_file_path = os.path.join(settings.NAROU_DATA_DIR_PATH, 'items_narou_ncode_spider_10.csv')
+    output_file_path = os.path.join(settings.NAROU_DATA_DIR_PATH, 'novel_meta.json')
+    error_log_file_path = os.path.join(settings.NAROU_DATA_DIR_PATH, 'fetch_error_log.txt')
+    fetch_novel_meta_from_ncode(csv_file_path, output_file_path, error_log_file_path)
+
+
 
