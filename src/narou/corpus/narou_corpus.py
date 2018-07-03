798c9375da9dbf92ce387b24c465027a8b14176a
Keisuke Kiriyama <krkr.xm.rkr7x@gmail.com>
2018-07-03 21:15:39 +0900

    :heavy_plus_sign: とりあえず学習が回る状態

--- src/narou/corpus/narou_corpus.py
+++ src/narou/corpus/narou_corpus.py
@@ -102,22 +102,24 @@ class NarouCorpus:
                             self.morph_set.add(morph)
                     wakati_sentences.append(wakati_line)
         wakati_sentences.append([' '])
-        self.wakati_sentences = wakati_sentences
 
-        # メタ情報に使われている単語も形態素集合に追加する
+        # タイトルとあらすじに使われている形態素を集合に追加
+        # あらすじはWord2Vec学習データに追加
         for i, meta_file_path in enumerate(self.meta_file_paths):
             print('meta process progress: {}'.format(i / len(self.meta_file_paths)))
             meta = self.load(meta_file_path)
             title = meta['title']
             title_morphs = self.wakati(title).split()
-            synopsis = meta['story']
-            synopsis_morphs = self.wakati(synopsis).split()
             for title_morph in title_morphs:
                 if not title_morph in self.morph_set:
                     self.morph_set.add(title_morph)
+            synopsis = meta['story']
+            synopsis_morphs = self.wakati(synopsis).split()
+            wakati_sentences.append(synopsis_morphs)
             for synopsis_morph in synopsis_morphs:
                 if not synopsis_morph in self.morph_set:
                     self.morph_set.add(synopsis_morph)
+        self.wakati_sentences = wakati_sentences
 
 
     def embedding(self):
@@ -131,23 +133,39 @@ class NarouCorpus:
         model.save(model_output_path)
         return model
 
-    def contents_to_tensor(self, words_max_length):
-        # 小説本文をテンソルに変換する
-        # shape=(小説数, 単語数, 単語ベクトルサイズ)
-        # 1話目のみのテンソルを作成
+    def data_to_tensor_emb_idx(self, contents_length, synopsis_length=300):
+        # 小説本文とあらすじのデータをテンソルに変換する
+        # 小説本文: shape=(小説数, 単語数, 単語ベクトルサイズ)
+        # あらすじ: shape=(小説数, 単語数, 形態素インデックス: 1)
         # words_max_length: 使用する単語数
-        X = np.zeros((len(self.contents_file_paths), words_max_length, self.embedding_size))
-        for novel_index, contents in enumerate([self.contents_from_file_path(contents_file_path) for contents_file_path in self.contents_file_paths]):
-            #　分かち書きを揃えるためにlineごとにwakatiを行う
+        X = np.zeros((len(self.contents_file_paths), contents_length, self.embedding_size), dtype=np.float)
+        Y = np.zeros((len(self.contents_file_paths), synopsis_length, 1), dtype=np.float)
+        for novel_index, contents_file_path in enumerate(self.contents_file_paths):
+            contents = self.contents_from_file_path(contents_file_path)
+            # 小説本文は1話目のみのテンソルを作成
+            # 本文は分かち書きを揃えるためにlineごとにwakatiを行う
             episode_one_wakati_lines = [self.cleaning(self.wakati(line)) for line in contents[0]]
-            morphs = ' '.join(episode_one_wakati_lines).split()
-            morphs_to_embed = morphs[0:words_max_length] if len(morphs) > words_max_length else (self.padding(morphs, words_max_length))
-            for morph_index, morph in enumerate(morphs_to_embed):
+            contents_morphs = ' '.join(episode_one_wakati_lines).split()
+            contents_morphs_to_embed = contents_morphs[0:contents_length] if len(contents_morphs) > contents_length else (self.padding(contents_morphs, contents_length))
+            for contents_morph_index, contents_morph in enumerate(contents_morphs_to_embed):
                 try:
-                    X[novel_index][morph_index] = self.embedding_model.__dict__['wv'][morph]
+                    X[novel_index][contents_morph_index] = self.embedding_model.__dict__['wv'][contents_morph]
                 except:
-                    print('[ERROR]error in contents_to_tensor: {}'.format(morph))
-        return X
+                    print('[ERROR]error in contents_to_tensor: {}'.format(contents_morph))
+
+            # あらすじは形態素のインデックスによるテンソル
+            ncode = contents_file_path.split('/')[-1].split('.')[0]
+            meta_file_path = os.path.join(settings.NAROU_DATA_DIR_PATH, 'meta', ncode + '_meta.json')
+            synopsis = self.synopsis_from_file_path(meta_file_path)
+            synopsis_morphs = self.wakati(synopsis).split()
+            synopsis_morphs_to_idx = self.padding(synopsis_morphs, synopsis_length)
+            for synopsis_morph_index, synopsis_morph in enumerate(synopsis_morphs_to_idx):
+                try:
+                    Y[novel_index][synopsis_morph_index][0] = self.morph_indices[synopsis_morph] / len(self.morph_indices)
+                except:
+                    print('[ERROR]error in synopsis_to_tensor: {}'.format(synopsis_morph))
+        return X, Y
+
 
     def padding(self, morphs, maxlen):
         for _ in range(maxlen - len(morphs)):
@@ -163,5 +181,5 @@ def test_embedding():
 
 if __name__ == '__main__':
     corpus = NarouCorpus()
-    corpus.contents_to_tensor(1000)
+    X,Y = corpus.data_to_tensor_emb_idx(1000)
 
--- src/narou/generation/generate_from_morph_index.py
+++ src/narou/generation/generate_from_morph_index.py
@@ -1,11 +1,62 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, RepeatVector
+from keras.layers.recurrent import LSTM
+from keras.layers.wrappers import TimeDistributed
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 from src.narou.corpus.narou_corpus import NarouCorpus
+
+
+def train():
+    corpus = NarouCorpus()
+    N = len(corpus.contents_file_paths)
+    N_train = int(N * 0.9)
+    # N_validation = N - N_train
+    contents_length = 1000
+    synopsis_length = 300
+    X, Y = corpus.data_to_tensor_emb_idx(contents_length=contents_length, synopsis_length=synopsis_length)
+    X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, train_size = N_train)
+
+    # モデル設定
+    n_in = corpus.embedding_size
+    n_hidden = 128
+    n_out = 1
+
+    model = Sequential()
+
+    # Encoder
+
+    model.add(LSTM(n_hidden, input_shape=(contents_length, n_in)))
+
+    # Decoder
+
+    model.add(RepeatVector(synopsis_length))
+    model.add(LSTM(n_hidden, return_sequences=True))
+
+    model.add(TimeDistributed(Dense(n_out)))
+    model.add(Activation('linear'))
+    model.compile(loss='sparse_categorical_crossentropy', # 要検討
+                  optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999),
+                  metrics=['accuracy'])
+
+    # モデル学習
+    epochs = 200
+    batch_size = 5
+
+    hist = model.fit(X_train, Y_train,
+                     batch_size=batch_size,
+                     epochs=epochs,
+                     validation_data=(X_validation, Y_validation))
+
+    # for epoch in range(epochs):
+    #     model.fit(X_train, Y_train, batch_size=batch_size, epochs=1,
+    #               validation_data=(X_validation, Y_validation))
+    return hist
 
 
 
 if __name__ == '__main__':
-    corpus = NarouCorpus()
-    corpus.contents_to_tensor(0)
-    # print(corpus.contents(ncode='n1161ds'))
-    # print(repr(corpus.synopsis(ncode='n1161ds')))
-    # print(corpus.contents_from_file_path('/Users/kiriyama.k.ab/Fujii.lab/story_modeling/data/narou/meta/n1875cl_meta.json'))
-    # print(corpus.synopsis_from_file_path('/Users/kiriyama.k.ab/Fujii.lab/story_modeling/data/narou/meta/n1875cl_meta.json'))
+    train()
